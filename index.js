80006aa272dd245871382792c2fa3f029c22c1d7
Abhimanyu Gupta <abhimanyugupta248@gmail.com>
2023-09-02 14:39:28 +0530

    split tracking added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let amount = document.getElementById("Amount");
 let date = document.getElementById("Date");
 let message = document.getElementById("Message");
+let split = document.getElementById("split");
 let bal = document.getElementById("balance");
 let btn = document.getElementById("addbtn");
 let addbal = document.getElementById("addbal");
@@ -12,7 +13,8 @@ btn.addEventListener("click", () => {
     let d = localStorage.getItem('d');
     let msg = localStorage.getItem('msg');
     let paisa =localStorage.getItem('paisa');
-    if (amount.value == "" || date.value == "" || message.value == "" ||  bal.innerText==0) {
+    let sp =localStorage.getItem('sp');
+    if (amount.value == "" || date.value == "" || message.value == "" ||  bal.innerText==0 ||split.value =="") {
         alert("Either field is empty or Balance is zero");
     }
     else {
@@ -20,19 +22,21 @@ btn.addEventListener("click", () => {
             amountobj = [];
             dateobj = [];
             msgobj = [];
+            spobj=[];
         }
         else {
             amountobj = JSON.parse(a);
             dateobj = JSON.parse(d);
             msgobj = JSON.parse(msg);
             paisaobj =JSON.parse(paisa);
+            spobj = JSON.parse(sp);
 
         }
 
         let leftbal = +bal.innerText - amount.value;
         bal.innerText = leftbal;
         
-        paisaobj[0]=leftbal;
+        paisaobj[0]=leftbal.toFixed(2);
         if(amount.value>0){
             amountobj.push(amount.value);
         }
@@ -42,15 +46,18 @@ btn.addEventListener("click", () => {
         }
         dateobj.push(date.value);
         msgobj.push(message.value);
+        spobj.push(split.value);
 
         localStorage.setItem('a', JSON.stringify(amountobj));
         localStorage.setItem('d', JSON.stringify(dateobj));
         localStorage.setItem('msg', JSON.stringify(msgobj));
-        localStorage.setItem('paisa',JSON.stringify(paisaobj))
+        localStorage.setItem('sp', JSON.stringify(spobj));
+        localStorage.setItem('paisa',JSON.stringify(paisaobj));
 
         amount.value = "";
         date.value = "";
         message.value = "";
+        split.value="";
         showtrans();
     }
 });
@@ -60,6 +67,7 @@ function showtrans() {
     d = localStorage.getItem('d');
     msg = localStorage.getItem('msg');
     paisa =localStorage.getItem('paisa');
+    sp =localStorage.getItem('sp');
     if(paisa==undefined)
     {
         bal.innerText=0;
@@ -68,11 +76,13 @@ function showtrans() {
         amountobj = [];
         dateobj = [];
         msgobj = [];
+        spobj=[];
     }
     else {
         amountobj = JSON.parse(a);
         dateobj = JSON.parse(d);
         msgobj = JSON.parse(msg);
+        spobj=JSON.parse(sp);
     }
     paisaobj =JSON.parse(paisa);
     if(paisa==undefined)
@@ -87,7 +97,7 @@ function showtrans() {
     for (i = len-1; i >= 0; i--) {
             html+=`<tr>
             <td>${dateobj[i]}</td>
-            <td>${amountobj[i]}</td>
+            <td>${spobj[i]==1?amountobj[i]:amountobj[i]+"/"+(amountobj[i]/spobj[i]).toFixed(2)}</td>
             <td>${msgobj[i]}</td>
         </tr>`
     }
@@ -97,12 +107,25 @@ function showtrans() {
     amountobj.forEach(index => {
         totalspent+=parseFloat(index);
     });
+    let myspent=0;
+    for(let i=0;i<amountobj.length;i++){
+        let am =parseFloat(amountobj[i]);
+        let spent = am/parseFloat(spobj[i]);
+        myspent+=spent;
+    }
     let tspent = document.getElementById("tspent");
-    let spent = document.querySelector(".spent")
+    let spent = document.querySelector(".spent");
+    let mspent = document.getElementById("mspent");
+    let mspen = document.querySelector(".mspen");
     tspent.innerHTML = totalspent.toFixed(2);
+    mspent.innerHTML = myspent.toFixed(2);
     if(totalspent==0)
     {
         spent.style.display='none';
+    }
+    if(myspent==0)
+    {
+        mspen.style.display='none';
     }
 }
 
@@ -153,6 +176,7 @@ cdata.addEventListener('click',()=>{
     localStorage.removeItem("d");
     localStorage.removeItem("msg");
     localStorage.removeItem("paisa");
+    localStorage.removeItem("sp");
     location.reload()
     }
 })
